aad56e545d277c24acf98f6d2fc3f0fd89d57b07
mako <coco.jasmine18@icloud.com>
2020-06-09 05:20:42 +0000

    [fix]header footer

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+
+  before_action :authenticate_user!
+  
   def index
     @posts = Post.all
   end
@@ -9,7 +12,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-
+    @post.user_id = current_user.id
     if@post.save
       redirect_to posts_path
     else
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e54b592d28ee26089896e62021994b3f7ca97c0dcb0a3fa91253ec6816df8fb49fa512ff1871d6f9a126467c7b5217b4aa9cd2c82e6f1da3242bdc8819590a7b'
