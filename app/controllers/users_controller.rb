a3f7badc2ac7a6f8703924849b225a197668ed73
mako <coco.jasmine18@icloud.com>
2020-06-12 17:29:42 +0000

    [add]user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,21 +3,30 @@ class UsersController < ApplicationController
 
   def index
   end
-  
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.all
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   def out
   end
 
   def quit
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
