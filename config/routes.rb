a8f3680a7fc07e0657a385757f71d0eace391297
mako <coco.jasmine18@icloud.com>
2020-06-24 08:03:30 +0000

    [add] mailer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get   'inquiry'         => 'inquiry#index'     # 入力画面
+  post  'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
+  post  'inquiry/thanks'  => 'inquiry#thanks'    # 送信完了画面
+
   devise_for :users, controllers: {   registrations: 'users/registrations',
                                       sessions: 'users/sessions' }
 
