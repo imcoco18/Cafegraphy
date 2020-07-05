1291a49d65be4f50c607db43fe089724da8ad9cd
mako <coco.jasmine18@icloud.com>
2020-07-05 15:31:29 +0000

    [add] search tags

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -52,6 +52,7 @@ class PostsController < ApplicationController
     #キーワード
     @q = Post.ransack(params[:q])
     @results = @q.result(distinct: true).order("created_at DESC").page(params[:page]).per(5)
+    @posts = params[:tag_id].present? ? Tag.find(params[:tag_id]).posts : Post.all.order("created_at DESC").page(params[:page]).per(5)
   end
 
   def tags_index
